56fa0a60059ebda546e798d65a5e7f03165f704f
Damian Owerko <Damowerko@users.noreply.github.com>
2023-03-10 11:41:38 -0500

    test_generation wip

--- scripts/test_generalization.py
+++ scripts/test_generalization.py
@@ -1,14 +1,16 @@
 import argparse
 import os
-from tempfile import TemporaryDirectory
-from typing import Dict, List, Tuple
+from typing import Dict, List
 
 import pandas as pd
+import torch
 from tqdm import tqdm
 
 import wandb
-from mtt.data import StackedImageData, build_test_datapipe
-from mtt.models import Conv2dCoder
+from mtt.data import StackedImageData, build_test_datapipe, collate_fn
+from mtt.models import Conv2dCoder, load_model
+from mtt.peaks import find_peaks
+from mtt.utils import compute_ospa
 
 
 def main():
@@ -36,13 +38,15 @@ def main():
 
     model, name = load_model(args.model_uri)
     results: List[pd.DataFrame] = []
-    for scale_dir in os.listdir(args.data_dir):
-        print(f"Testing model on {scale_dir}")
+    for scale in range(1, 5):
+        print(f"Testing model on {scale}km")
         result = test_model(
-            model, os.path.join(args.data_dir, scale_dir, "simulations.pkl")
+            model,
+            os.path.join(args.data_dir, f"{scale}km", "simulations.pkl"),
+            scale=scale,
         )
         # data in data_dir is organized by scale in folders: 1km, 2km, 3km etc.
-        result["scale"] = int(scale_dir.split("km")[0])
+        result["scale"] = scale
         results.append(result)
     df = pd.concat(results)
     if not os.path.exists(args.output_dir):
@@ -50,24 +54,36 @@ def main():
     df.to_csv(os.path.join(args.output_dir, f"{name}.csv"), index=False)
 
 
-def test_model(model: Conv2dCoder, data_path: str):
+def test_model(model: Conv2dCoder, data_path: str, scale: int = 1):
     model = model.cuda()
     datapipe = build_test_datapipe(
-        data_path, unbatch=False, vector_to_image_kwargs={"device": "cuda"}
+        data_path,
+        unbatch=False,
+        vector_to_image_kwargs=dict(device="cuda", img_size=128 * scale),
     )
     result: List[Dict] = []
     for simulation_idx, simulation in enumerate(tqdm(datapipe, total=len(datapipe))):
         for step_idx, data in enumerate(simulation):
-            assert isinstance(data, StackedImageData)
+            data: StackedImageData = data
+            with torch.no_grad():
+                output = model.forward(data.sensor_images.cuda())
 
-            output = model.forward(data.sensor_images.cuda())
-            mse = model.loss(output, data.target_images[-1:].cuda()).item()
-
+            mse = (output[-1] - data.target_images[-1]).pow(2).mean().item()
             cardinality_target = model.cardinality_from_image(
-                data.target_images[-1:]
+                data.target_images[-1]
             ).item()
-            cardinality_output = model.cardinality_from_image(output[-1:]).item()
+            cardinality_output = model.cardinality_from_image(output).item()
             cardinality_truth = len(data.info[-1]["target_positions"])
+
+            # targets_truth = data.info[-1]["target_positions"]
+            # targets_gmm = find_peaks(
+            #     output[-1].cpu().numpy(), width=data.info[-1]["window"], model="gmm"
+            # ).means
+            # targets_kmeans = find_peaks(
+            #     output[-1].cpu().numpy(), width=data.info[-1]["window"], model="kmeans"
+            # ).means
+            # ospa_gmm = compute_ospa(targets_truth, targets_gmm, 500)
+            # ospa_kmeans = compute_ospa(targets_truth, targets_kmeans, 500)
 
             result.append(
                 dict(
@@ -77,6 +93,8 @@ def test_model(model: Conv2dCoder, data_path: str):
                     cardinality_target=cardinality_target,
                     cardinality_output=cardinality_output,
                     cardinality_truth=cardinality_truth,
+                    # ospa_gmm=ospa_gmm,
+                    # ospa_kmeans=ospa_kmeans,
                 )
             )
     return pd.DataFrame(result)
