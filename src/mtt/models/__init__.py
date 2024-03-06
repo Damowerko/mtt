ef43afbde856f4d930ad16a5be9514d2961ffcc5
Damian Owerko <Damowerko@users.noreply.github.com>
2024-03-06 10:25:00 -0500

    add EncoderDecoder to models init

--- src/mtt/models/__init__.py
+++ src/mtt/models/__init__.py
@@ -1,4 +1,4 @@
-from mtt.models.convolutional import Conv2dCoder
+from mtt.models.convolutional import Conv2dCoder, EncoderDecoder
 from mtt.models.kernel import KNN
 from mtt.models.sparse import SparseBase
 from mtt.models.transformer import SparseBase, SpatialTransformer
