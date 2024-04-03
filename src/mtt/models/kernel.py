5e20268de3f3c3c5d514d7a71164ced9760e3306
Damian Owerko <damowerko@gmail.com>
2024-04-03 11:06:33 -0400

    kernel encoder decoder

--- src/mtt/models/kernel.py
+++ src/mtt/models/kernel.py
@@ -2,10 +2,253 @@ import typing
 
 import pytorch_lightning as pl
 import torch
-import torchcps.kernel.nn as knn
+import torch.nn as nn
+from torch_geometric.nn import MLP
+from torchcps.kernel.nn import GaussianKernel, KernelConv, KernelSample, Mixture
 
 from mtt.models.sparse import SparseBase, SparseOutput
 from mtt.utils import add_model_specific_args
+
+
+class KernelEncoderLayer(nn.Module):
+    def __init__(
+        self,
+        pos_dim: int,
+        max_filter_kernels: int,
+        n_channels: int,
+        n_channels_mlp: int,
+        sigma: float,
+        update_positions: bool,
+    ):
+        super().__init__()
+        self.conv = KernelConv(
+            max_filter_kernels,
+            n_channels,
+            n_channels,
+            pos_dim,
+            kernel_spread=sigma,
+            update_positions=update_positions,
+            kernel_init="grid",
+        )
+        self.sample = KernelSample(
+            kernel=GaussianKernel(sigma), nonlinearity=nn.LeakyReLU(), normalize=False
+        )
+        self.conv_norm = nn.BatchNorm1d(n_channels)
+        self.mlp = MLP(
+            [n_channels, n_channels_mlp, n_channels + pos_dim],
+            act=nn.LeakyReLU(),
+            norm="batch_norm",
+            act_first=True,
+        )
+        self.nonlinearity = nn.LeakyReLU()
+
+    def forward(self, x_in: Mixture):
+        x = self.conv.forward(x_in)
+        # KernelSample applies the nonlinearity after sampling
+        x = self.sample.forward(x, x_in.positions, x_in.batch)
+        x = x.map_weights(self.conv_norm)
+        mlp_out = self.mlp.forward(x.weights)
+        delta_positions, delta_weights = torch.split(
+            mlp_out, [x.positions.shape[-1], x.weights.shape[-1]], dim=-1
+        )
+        x_out = Mixture(
+            x.positions + delta_positions,
+            x.weights + delta_weights,
+            x.batch,
+        )
+        return x_out
+
+
+class KernelDecoderLayer(nn.Module):
+    def __init__(
+        self,
+        pos_dim: int,
+        max_filter_kernels: int,
+        n_channels: int,
+        n_channels_mlp: int,
+        sigma_cross: float,
+        sigma_self: float,
+        update_positions: bool,
+    ):
+        super().__init__()
+        self.cross_conv = KernelConv(
+            max_filter_kernels,
+            n_channels,
+            n_channels,
+            pos_dim,
+            kernel_spread=sigma_cross,
+            update_positions=update_positions,
+            kernel_init="grid",
+        )
+        self.sample_cross = KernelSample(
+            kernel=GaussianKernel(sigma_cross),
+            nonlinearity=nn.LeakyReLU(),
+            normalize=False,
+        )
+        self.norm_cross = nn.BatchNorm1d(n_channels)
+        self.conv_self = KernelConv(
+            max_filter_kernels,
+            n_channels,
+            n_channels,
+            pos_dim,
+            kernel_spread=sigma_self,
+            update_positions=update_positions,
+            kernel_init="grid",
+        )
+        self.sample_self = KernelSample(
+            kernel=GaussianKernel(sigma_self),
+            nonlinearity=nn.LeakyReLU(),
+            normalize=False,
+        )
+        self.norm_self = nn.BatchNorm1d(n_channels)
+        self.mlp_self = MLP(
+            [n_channels, n_channels_mlp, n_channels + pos_dim],
+            act=nn.LeakyReLU(),
+            norm="batch_norm",
+            act_first=True,
+            plain_last=True,
+        )
+
+    def forward(self, z: Mixture, e: Mixture):
+        # convolve e and sample at z.positions
+        e_conv = self.cross_conv.forward(e)
+        e_sampled = self.sample_cross.forward(e_conv, z.positions, z.batch)
+        e_sampled = e_sampled.map_weights(self.norm_cross)
+
+        # z = z + e_sampled
+        z = z.map_weights(e_sampled.weights.add)
+        # z -> conv -> sample -> norm -> +z
+        z_conv = self.conv_self.forward(z)
+        z_sampled = self.sample_self.forward(z_conv, z.positions, z.batch)
+        z_sampled = z_sampled.map_weights(self.norm_self)
+        z = z.map_weights(z_sampled.weights.add)
+        # use mlp to update z weights and positions
+        mlp_out = self.mlp_self.forward(z.weights)
+        delta_positions, delta_weights = torch.split(
+            mlp_out, [z.positions.shape[-1], z.weights.shape[-1]], dim=-1
+        )
+        z = Mixture(
+            z.positions + delta_positions,
+            z.weights + delta_weights,
+            z.batch,
+        )
+        return z
+
+
+class KNNBlock(nn.Module):
+    def __init__(
+        self,
+        conv: KernelConv,
+        sample: KernelSample,
+        norm: nn.Module,
+        delta_module: nn.Module,
+    ):
+        super().__init__()
+        self.conv = conv
+        self.sample = sample
+        self.norm = norm
+        self.delta_module = delta_module
+
+    def forward(
+        self,
+        x: Mixture,
+    ):
+        # convolution and sampling operation
+        y = self.conv.forward(x)
+        y = y.map_weights(self.norm)
+        y = self.sample.forward(y, x.positions, x.batch)
+
+        # pointwise update of the kernel weights and positions
+        delta_out = self.delta_module.forward(x.weights)
+        delta_positions, delta_weights = torch.split(
+            delta_out, [x.positions.shape[-1], x.weights.shape[-1]], dim=-1
+        )
+        y = Mixture(
+            x.positions + delta_positions,
+            x.weights + delta_weights,
+            x.batch,
+        )
+        return y
+
+
+class KNNModule(nn.Module):
+    def __init__(
+        self,
+        n_dimensions: int,
+        in_channels: int,
+        hidden_channels: int,
+        out_channels: int,
+        n_layers: int,
+        n_layers_mlp: int,
+        hidden_channels_mlp: int,
+        sigma: float | typing.Sequence[float],
+        max_filter_kernels: int,
+        update_positions: bool,
+        alpha: float | None = None,
+        kernel_init: str = "uniform",
+    ):
+        super().__init__()
+        self.n_layers = n_layers
+        self.nonlinearity = nn.LeakyReLU()
+
+        self.readin = MLP(
+            in_channels=in_channels,
+            hidden_channels=hidden_channels_mlp,
+            out_channels=hidden_channels,
+            num_layers=n_layers_mlp,
+            act=self.nonlinearity,
+            norm="batch_norm",
+            plain_last=False,
+        )
+        self.readout = MLP(
+            in_channels=hidden_channels,
+            hidden_channels=hidden_channels_mlp,
+            out_channels=out_channels,
+            num_layers=n_layers_mlp,
+            act=self.nonlinearity,
+            norm="batch_norm",
+            plain_last=True,
+        )
+        sigma = sigma if isinstance(sigma, typing.Sequence) else [sigma] * n_layers
+
+        blocks: list[KNNBlock] = []
+        for l in range(self.n_layers):
+            blocks += [
+                KNNBlock(
+                    conv=KernelConv(
+                        max_filter_kernels=max_filter_kernels,
+                        in_channels=hidden_channels,
+                        out_channels=hidden_channels,
+                        n_dimensions=n_dimensions,
+                        kernel_spread=sigma[l],
+                        update_positions=update_positions,
+                        kernel_init=kernel_init,
+                    ),
+                    sample=KernelSample(
+                        kernel=GaussianKernel(sigma[l]),
+                        alpha=alpha,
+                        nonlinearity=nn.LeakyReLU(),
+                    ),
+                    norm=nn.BatchNorm1d(hidden_channels),
+                    delta_module=MLP(
+                        in_channels=hidden_channels,
+                        hidden_channels=hidden_channels_mlp,
+                        out_channels=hidden_channels + n_dimensions,
+                        num_layers=n_layers_mlp,
+                        act=self.nonlinearity,
+                        norm="batch_norm",
+                        plain_last=True,
+                    ),
+                )
+            ]
+        self.blocks = nn.ModuleList(blocks)
+
+    def forward(self, x: Mixture):
+        x = x.map_weights(self.readin.forward)
+        for l in range(self.n_layers):
+            x = typing.cast(KNNBlock, self.blocks[l]).forward(x)
+        x = x.map_weights(self.readout.forward)
+        return x
 
 
 class KNN(SparseBase):
@@ -30,9 +273,9 @@ class KNN(SparseBase):
     ):
         super().__init__(**kwargs)
         self.save_hyperparameters()
-        self.model = knn.KNN(
+        self.model = KNNModule(
             pos_dim,
-            in_channels=measurement_dim + self.time_dim,
+            in_channels=measurement_dim * self.input_length,
             hidden_channels=hidden_channels,
             out_channels=state_dim + 1,
             n_layers=n_layers,
@@ -45,7 +288,7 @@ class KNN(SparseBase):
         )
 
     def _forward(self, x: torch.Tensor, x_pos: torch.Tensor, x_batch: torch.Tensor):
-        x_mixture = knn.Mixture(x_pos, x, x_batch)
+        x_mixture = Mixture(x_pos, x, x_batch)
         y_mixture = self.model.forward(x_mixture)
         mu = y_mixture.positions
         sigma = y_mixture.weights[:, :-1]
